a19a82e9d057baaf29eee4c82bfa436458bd429f
jihuayu <i@jihuayu.site>
2020-12-05 13:56:45 +0800

    upload

---
+++ b.ts
@@ -0,0 +1,39 @@
+import got from "got";
+import {mkdirSync, writeFileSync} from "fs";
+import {extname} from "path";
+
+const cheerio = require('cheerio');
+
+const url = `https://asiansister.com/`
+
+process.nextTick(async () => {
+    for (let index = Number(1000); index < Number(1003); index++) {
+        let num = 0;
+        while (++num) {
+            if (num > 100) {
+                break;
+            }
+            mkdirSync(`pic/${index}/`, {recursive: true})
+            try {
+                const nownum = num;
+                const ret = await got.get(`${url}viewImg.php?code=${index}&id=${num}`);
+                const $ = cheerio.load(ret.body)
+                const path = $("meta[property='og:image']").attr("content")
+                if (!path) {
+                    break;
+                }
+                try {
+                    console.log(`${url}/${path}`)
+                    const ret = await got.get(`${url}/${path}`);
+                    const r = extname(path)
+                    writeFileSync(`./pic/${index}/${nownum}${r}`, ret.rawBody, {encoding: 'binary'})
+                } catch (e) {
+                    console.error(`error download ${index} ${nownum}`)
+                }
+            } catch (e) {
+                break;
+            }
+        }
+    }
+
+})
